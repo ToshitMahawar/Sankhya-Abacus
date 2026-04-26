62134871614a6694cc35651ba87485e384f9017c
TOSHIT MAHAWAR <toshitmahawar@TOSHITs-MacBook-Air.local>
2026-04-26 11:47:23 +0530

    updated navbar and UI

--- public/js/abacus.js
+++ public/js/abacus.js
@@ -119,4 +119,10 @@ clearBtn.addEventListener("click", () => {
 
     display.innerText = "0";
 });
+const toggle = document.querySelector(".toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
